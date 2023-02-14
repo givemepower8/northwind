241a64b1da1118aa5df7d1bba2c30a3dec31acff
Wang, Wei <wei.wang@ia.ca>
2023-02-14 11:14:24 -0800

    minor

---
+++ src/Core/Northwind.Application/Contracts/Infrastructure/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿using Northwind.Application.Models.Email;
+
+namespace Northwind.Application.Contracts.Infrastructure
+{
+    public interface IEmailService
+  {
+    Task<bool> SendEmail(Email email);
+  }
+}
---
+++ src/Core/Northwind.Application/Contracts/Persistence/IProductRepository.cs
@@ -0,0 +1,8 @@
+﻿using Northwind.Domain.Entities;
+
+namespace Northwind.Application.Contracts.Persistence
+{
+  public interface IProductRepository : IAsyncRepository<Product>
+  {
+  }
+}
