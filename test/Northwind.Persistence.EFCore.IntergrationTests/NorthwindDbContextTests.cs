5360df792ac384aee11399d531d7933190442549
Wang, Wei <wei.wang@ia.ca>
2023-02-14 17:22:40 -0800

    add ef core test

---
+++ test/Northwind.Persistence.EFCore.IntergrationTests/NorthwindDbContextTests.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using Northwind.Domain.Entities;
+
+namespace Northwind.Persistence.EFCore.IntergrationTests
+{
+  public class NorthwindDbContextTests
+  {
+    [Fact]
+    public void DbContextAddCategory()
+    {
+      var dbContextOptions = new DbContextOptionsBuilder<NorthwindDbContext>()
+        .UseInMemoryDatabase(Guid.NewGuid().ToString()).Options;
+
+      NorthwindDbContext db = new NorthwindDbContext(dbContextOptions);
+
+      var countBefore = db.Categories.Count();
+      var category = new Category { CategoryName = "Test", Description = "test", CategoryId = 1 };
+      db.Categories.Add(category);
+      db.SaveChanges();
+      var countAfter = db.Categories.Count();
+      Assert.Equal(countBefore + 1, countAfter);
+    }
+  }
+}
