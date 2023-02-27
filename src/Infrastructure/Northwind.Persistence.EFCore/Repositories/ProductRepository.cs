72ba08e1e421379ee6320f1070b46209a49cf14e
Wang, Wei <wei.wang@ia.ca>
2023-02-27 14:32:07 -0800

    add ProductRepository

---
+++ src/Infrastructure/Northwind.Persistence.EFCore/Repositories/ProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using Northwind.Domain.Entities;
+using Northwind.Application.Contracts.Persistence;
+
+namespace Northwind.Persistence.EFCore.Repositories
+{
+	public class ProductRepository : BaseRepository<Product>, IProductRepository
+	{
+		public ProductRepository(NorthwindDbContext dbContext) : base(dbContext)
+		{
+		}
+	}
+}
