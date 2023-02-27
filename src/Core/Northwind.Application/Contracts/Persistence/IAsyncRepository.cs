94dad7699956b80007142e5fba8b1ea999d6d61a
Wang, Wei <wei.wang@ia.ca>
2023-02-27 14:28:57 -0800

    add base repository

--- src/Core/Northwind.Application/Contracts/Persistence/IAsyncRepository.cs
+++ src/Core/Northwind.Application/Contracts/Persistence/IAsyncRepository.cs
@@ -1,17 +1,27 @@
 ﻿namespace Northwind.Application.Contracts.Persistence
 {
-  /// <summary>
-  /// The general repository for CRUD operations used by application
-  /// leave the concrete implementation to whatever EF core / Azure Cosmos / MongoDB
-  /// </summary>
-  /// <typeparam name="T"></typeparam>
-  public interface IAsyncRepository<T> where T : class
-  {
-    Task<T?> GetByIdAsync(int id);
-    Task<IReadOnlyList<T>> ListAllAsync();
-    Task<T> AddAsync(T entity);
-    Task UpdateAsync(T entity);
-    Task DeleteAsync(T entity);
-    Task<IReadOnlyList<T>> GetPagedResponseAsync(int page, int size);
-  }
+	/// <summary>
+	/// The general repository for CRUD async operations used by application
+	/// leave the concrete implementation to whatever EF core / Azure Cosmos / MongoDB
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	public interface IAsyncRepository<T> where T : class
+	{
+		/// <summary>
+		/// All repository of T manage entities which has Id
+		/// </summary>
+		/// <param name="id"></param>
+		/// <returns></returns>
+		Task<T?> GetByIdAsync(int id);
+
+		Task<IReadOnlyList<T>> ListAllAsync();
+		
+		Task<T> AddAsync(T entity);
+		
+		Task UpdateAsync(T entity);
+		
+		Task DeleteAsync(T entity);
+		
+		Task<IReadOnlyList<T>> GetPagedListAsync(int page, int size);
+	}
 }
---
+++ src/Infrastructure/Northwind.Persistence.EFCore/Repositories/BaseRepository.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Northwind.Application.Contracts.Persistence;
+
+namespace Northwind.Persistence.EFCore.Repositories
+{
+	public class BaseRepository<T> : IAsyncRepository<T> where T : class
+	{
+
+		protected readonly NorthwindDbContext DbContext;
+
+		public BaseRepository(NorthwindDbContext dbContext)
+		{
+			DbContext = dbContext;
+		}
+
+		public virtual async Task<T?> GetByIdAsync(int id)
+		{
+			T? t = await DbContext.Set<T>().FindAsync(id);
+			return t;
+		}
+
+		public virtual async Task<IReadOnlyList<T>> ListAllAsync()
+		{
+			return await DbContext.Set<T>().ToListAsync();
+		}
+
+		public virtual async Task<IReadOnlyList<T>> GetPagedListAsync(int page, int size)
+		{
+			return await DbContext.Set<T>().Skip((page - 1) * size).Take(size).AsNoTracking().ToListAsync();
+		}
+
+		public virtual async Task<T> AddAsync(T entity)
+		{
+			await DbContext.Set<T>().AddAsync(entity);
+			await DbContext.SaveChangesAsync();
+
+			return entity;
+		}
+
+		public virtual async Task UpdateAsync(T entity)
+		{
+			DbContext.Entry(entity).State = EntityState.Modified;
+			await DbContext.SaveChangesAsync();
+		}
+
+		public virtual async Task DeleteAsync(T entity)
+		{
+			DbContext.Set<T>().Remove(entity);
+			await DbContext.SaveChangesAsync();
+		}
+	}
+}
